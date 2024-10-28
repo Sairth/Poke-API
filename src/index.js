833356f3109290a3567ba927eef206ba70ad9108
Arthur De Aquino <Sairth0@gmail.com>
2024-10-28 14:10:13 -0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const app = express()
+const port = 3000
+
 const PokemonRoutes = require("./routes/PokemonRoutes")
 const ElementRoutes = require("./routes/ElementRoutes")
 const GameRoutes = require("./routes/GameRoutes")
 const GenerationRoutes = require("./routes/GenerationRoutes")
 const GymRoutes = require("./routes/GymRoutes")
 const RegionRoutes = require("./routes/RegionRoutes")
-const port = 3000
 
 app.use(express.json())
 app.use('/pokemon', PokemonRoutes)
@@ -15,6 +16,7 @@ app.use('/game', GameRoutes)
 app.use('/generation', GenerationRoutes)
 app.use('/gym', GymRoutes)
 app.use('/region', RegionRoutes)
+
 
 app.listen(port, () => {
     console.log('Servidor rodando!')
