7377c4dabdeef0b6a14e6e5130bc40cafddbebd0
Arthur De Aquino <Sairth0@gmail.com>
2024-10-30 10:08:09 -0300

    Better conection then before and use of dotenv library

--- config/database.js
+++ config/database.js
@@ -1,16 +1,29 @@
+//Importa e armazena o sequelize em uma constante
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('pokedb', 'root', '', {
-    host: "localhost",
-    dialect: 'mysql'
-});
+//Importa as configurações da biblioteca dotenv
+require('dotenv').config()
 
+/*Estabelece uma conexão com o banco de dados inserido no arquivo .env 
+com as especificações necessarias*/ 
+const sequelize = new Sequelize(process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT
+    });
+
+/*Caso tenha sucesso na conexão retorna uma mensagem positiva em relação a conexão,
+caso falhe retorna uma mensagem negativa e o erro ocorrido*/
 sequelize.authenticate()
     .then(function() {
         console.log("Conectado com sucesso ao banco de dados pokedb");
     })
     .catch(function(erro) {
-        console.log("Falha ao se conectar ao banco de dados:", erro); // Corrigido o operador de concatenação
+        console.log("Falha ao se conectar ao banco de dados:", erro);
     });
 
+//Exporta o sequelize para que seja usado em outro arquivo
 module.exports = sequelize;
