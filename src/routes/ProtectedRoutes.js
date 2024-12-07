c98ff6d358742db3eb2e405d5c3c045f182fb8ae
Arthur De Aquino <Sairth0@gmail.com>
2024-12-06 22:13:45 -0300

    Feat: Add some comments

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -8,8 +8,13 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
+//Rota para criar um novo pokemon(Parâmetros necessários: name, description, rarity, stage, region, generation, weight, height)
 router.post('/pokemon/create', PokemonController.createpokemon)
+
+//Rota para criar uma nova região(Parâmetros necessários: name, description, teacher)
 router.post('/region/create', RegionController.createRegion)
+
+//Rota para adicionar um novo jogo(Parâmetros necessários: name, description, release)
 router.post('/game/create', GameController.createGame)
 
 module.exports = router;
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,6 +3,7 @@ const userController = require('../controller/UserController');
 
 const router = express.Router();
 
+//rota para o fazer login(Parâmetros necesários: username e password)
 router.post('/login', userController.login);
 router.post('/create', userController.createUser);
 
